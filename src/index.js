8ee29ec77267a21367eb6a7ab49c2527ec3ab685
tukaro <91900838+tukaro@users.noreply.github.com>
2023-02-12 23:21:24 +0700

    suaa

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path =require('path')
+var morgan = require('morgan');
+const  handlebars = require ('express-handlebars');
+const app = express();
+const port = 3000;
+ 
+ 
+app.use(morgan('combined'));
+app.engine('hbs', handlebars.engine({
+  extname:'.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resource/views'));
+
+
+
+app.get('/', (req, res) => {
+  res.render('home');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port htttp://localhost:${port}`);
+})
