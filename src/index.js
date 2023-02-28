6bfe2818e3e7ac11cdb8c6beae0bbb5134f943af
tukaro <91900838+tukaro@users.noreply.github.com>
2023-03-01 02:27:11 +0700

    headre

--- src/index.js
+++ src/index.js
@@ -5,13 +5,15 @@ const  handlebars = require ('express-handlebars');
 const app = express();
 const port = 3000;
   
- 
+app.use(express.static(path.join(__dirname,'public')))
+
 app.use(morgan('combined'));
 app.engine('hbs', handlebars.engine({
   extname:'.hbs'
 }));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resource/views'));
+
 
 
 
